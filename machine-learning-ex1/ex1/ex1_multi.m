2ee4aa149a4c47bfa27849ab1ce98622ba8f6448
Joe Booth <joe@joebooth.com>
2015-09-07 23:57:12 -0700

    ex1 3.3 Normal Equations

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -175,7 +175,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+houseEst = [1 1650 3];
+price = houseEst * theta;
 
 
 % ============================================================
