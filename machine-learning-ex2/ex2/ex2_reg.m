a91f08aafc96c43fc3c2f4d45344410c64366b22
Joe Booth <joe@joebooth.com>
2015-09-12 02:11:57 -0700

    ex3

--- machine-learning-ex2/ex2/ex2_reg.m
+++ machine-learning-ex2/ex2/ex2_reg.m
@@ -90,7 +90,7 @@ initial_theta = zeros(size(X, 2), 1);
 lambda = 1;
 
 % Set Options
-options = optimset('GradObj', 'on', 'MaxIter', 400);
+options = optimset('GradObj', 'on', 'MaxIter', 4000);
 
 % Optimize
 [theta, J, exit_flag] = ...
